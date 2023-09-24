2a5aef5e09bfb3ac596313fa216225247f7ceb4f
Bobbyyy <88208156+Bobbbyyyyy@users.noreply.github.com>
2023-09-24 22:54:06 +0200

    ☁️

---
+++ README.md
@@ -0,0 +1,58 @@
+-----
+
+<p align="center">
+<img src="https://cdn.discordapp.com/attachments/1028350993856991262/1155192344019214357/Volt_Nuker.png", width="200", height="200">
+</p>
+
+-----
+
+### <p align="center">☁️ Cloud PFPS ☁️</p>
+
+<br><br>
+<p align="center">
+<strong>
+Cloud pfps is an auto pfps sender bot
+</strong>
+</p>
+<br>
+
+-----
+
+### <p align="center">⭐ Features ⭐</p>
+
+```
++ Clean UI
++ Fast execution
++ Already over 33k pfps
+```
+
+-----
+
+### <p align="center">🌫️ Preview 🌫️</p>
+
+
+<p align="center">
+<img src="https://media.discordapp.net/attachments/1028350993856991262/1155604146284679208/Cloud.png">
+</p>
+   
+-----
+
+### <p align="center">⚙️ Installation ⚙️</p>
+
+<p align="center">
+<img src="https://media.discordapp.net/attachments/1028350993856991262/1155607685543239851/tuto.png">
+</p>
+   
+-----
+
+### <p align="center">💡 Ideas 💡</p>
+
+<p align="center"><strong><i>Feel free to make a pull request on this repository to submit any idea!</i></strong</p>
+
+<br><br>
+* More commands (banners)
+<br><br>
+
+-----
+
+### <p align="center">README template by billythegoat356</p>
---
+++ main.py
@@ -0,0 +1,75 @@
+from asyncore import loop
+import random
+import time
+import discord
+from discord.ext import commands
+import os
+from pystyle import *
+blue = Col.blue
+cyan = Col.cyan
+
+
+
+#------------CONFIG------------
+TOKEN = "MTE1NTEzOTkwOTg1ODkwMjAzNw.GIZFvn.WpqSQ-JwoVjMCWsJABSz1ziiGbr4HT4k8iiTpA"
+prefix = "."
+allowed = 1137704471154143354
+title = "Cloud Pfps"
+desc = ""
+footer = "github.com/Bobbbyyyyy"
+color = 0x92cace
+banner = """
+┏  ┏┓┓     ┓  ┓
+┃  ┃ ┃┏┓┓┏┏┫  ┃
+┛  ┗┛┗┗┛┗┻┗┻  ┗
+
+"""
+#------------------------------
+
+
+os.system("cls")
+os.system(f'title Cloud Pfps / Made by Bobbyyy')
+System.Size(70, 20)
+Cursor.HideCursor()
+tostop = 0
+intents = discord.Intents.all()
+pfpbot = commands.Bot(command_prefix=prefix,intents=intents)
+
+def randnum(fname):
+    lines=open(fname).read().splitlines()
+    return random.choice(lines)
+
+@pfpbot.event
+async def on_ready():
+    os.system('cls')
+    print(Colorate.Horizontal(Colors.blue_to_cyan, Center.XCenter(banner), 1))
+    print(cyan, Center.XCenter(f"Connected to {pfpbot.user}"))
+
+@pfpbot.command()
+async def stop(ctx):
+  global tostop
+  if ctx.author.id == allowed:
+    tostop += 1
+    await ctx.reply("`✅` | **Stopped AutoPfP**")
+    print(cyan, Center.XCenter(f"Stopped AutoPfP"))
+  else:
+    await ctx.reply("`⛔` | **You are not allowed to do that**")
+
+@pfpbot.command()
+async def start(ctx):
+    global tostop
+    if ctx.author.id == allowed:
+        tostop = 0
+        await ctx.reply("`✅` | **Started AutoPfP**")
+        print(cyan, Center.XCenter(f"Started AutoPfP"))
+        while tostop == 0:
+            
+            embed = discord.Embed(title = title,description = desc,color = color)
+            embed.set_image(url = randnum('pfps.txt'))
+            embed.set_footer(text = footer)
+            await ctx.send(embed=embed)
+            time.sleep(3)
+    else:
+        await ctx.send("`⛔` | **You are not allowed to do that**")
+
+pfpbot.run(TOKEN)
